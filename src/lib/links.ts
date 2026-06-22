33b538909d821b5f4514810faad213b4c9803f55
ahmedosamasawah <sawahahmedosama@gmail.com>
2026-06-22 14:41:47 +0400

    links: update resume url

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,4 +1,4 @@
 const resume_url =
-	'https://drive.google.com/file/d/1LsbsX5ZyTDuYujI_oJHXbbWc6Ixo_BEk/view?usp=sharing';
+	'https://drive.google.com/file/d/1AW9wA3bTFBonfuP9r9_ygmr3ulip8T_e/view?usp=sharing';
 
 export { resume_url };
